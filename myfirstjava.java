bd423fda9644d4e77908bdf60ee73a36bc37e18f
KaranJogdande <karanjogdande1@gmail.com>
2025-03-26 10:57:56 +0530

    Create myfirstjava.java

---
+++ myfirstjava.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+    // Main method: the entry point of the program
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+    }
+}
